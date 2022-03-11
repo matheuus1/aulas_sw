0786c3a34c9416136191ad8de85c1e8a0c02589a
matheuus1 <80324098+matheuus1@users.noreply.github.com>
2022-03-11 12:40:03 -0300

    Add files via upload

---
+++ media.php
@@ -0,0 +1,8 @@
+<?php
+    $num1 = $_REQUEST['num1'];
+    $num2 = $_REQUEST['num2'];
+    $num3 = $_REQUEST['num3'];
+    $media = ($num1 + $num2 + $num3) /3;
+    echo "A média dos números é: $media"
+    
+?>
